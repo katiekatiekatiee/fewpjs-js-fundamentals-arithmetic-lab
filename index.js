4277e5d5a2e86bd5c6f24c958f5243da8c8883e5
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-09-14 21:35:15 -0400

    commit tree d33ce7ecd9e652ec8fe9340315525f772fa84d44

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 100) + 1;
+
+const num3 = 16;
+const num4 = 6;
+
+const mod = num3 % num4;
+
+const max = Math.max(1, 6, 7, 13, 19, 20);
+
+
